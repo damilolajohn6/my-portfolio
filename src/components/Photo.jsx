f59678c84f7251f9ca43347b1d15841efd599c54
Damilola john <damilolajohn622@gmail.com>
2025-02-07 16:26:11 +0100

    hero image

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,7 +1,26 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+
 
 const Photo = () => {
   return (
-    <div>Photo</div>
+    <div>
+      <motion.div>
+        <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px]">
+          <Image
+            src='/assets.pics.png'
+            priority
+            quality={100}
+            fill
+            alt="hero-image"
+            className="object-contain"
+          />
+        </div>
+      </motion.div>
+    </div>
   )
 }
 
