dc5826f2e69f998617c371f37fef4f4bea7e8290
Damilola john <damilolajohn622@gmail.com>
2025-02-07 23:35:10 +0100

    service page

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -11,7 +11,11 @@ import {
 } from "react-icons/fa";
 import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
 
-
+const about = {
+  title: "About me",
+  description:
+    "I am a passionate Software Engineer with 3 years of experience in developing high-quality web applications, designing intuitive user interfaces, and crafting impactful brand identities. Proficient in Web Development, UI/UX Design, Logo Design, and Graphics Design, I specialize in building scalable solutions with modern technologies like Next.js, React, Python, and Node.js. I thrive in agile environments, ensuring seamless collaboration and efficient project execution. With a strong problem-solving mindset and a keen eye for design, I create engaging, functional, and visually stunning digital experiences.",
+};
 
 const ResumePage = () => {
   return <div>resume page</div>;
