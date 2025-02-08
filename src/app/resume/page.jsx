17ebb3b20a580352eadd5577a172f2a7dcd791ea
Damilola john <damilolajohn622@gmail.com>
2025-02-08 02:40:03 +0100

    education

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -248,7 +248,38 @@ const ResumePage = () => {
               </div>
             </TabsContent>
             <TabsContent value="education" className="w-full">
-              <p>Your education details here...</p>
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{education.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experience.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {education.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-auto p-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.institution}
+                          </h3>
+                          <span className="text-accent">{item.duration}</span>
+
+                          <div className=" items-center gap-3">
+                            <h3 className="text-xl max-w-[260px] min-h-[60px] text-center text-white/80 lg:text-left">
+                              {item.degree}
+                            </h3>
+                            <p className="text-white/60 mx-auto xl:mx-0">
+                              {item.highlights}
+                            </p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
             </TabsContent>
             <TabsContent value="skills" className="w-full">
               <p>Your skills details here...</p>
