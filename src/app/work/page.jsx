3a32c680b14cd1ede5bfd5f6cdb1e85cbfbb636d
Damilola john <damilolajohn622@gmail.com>
2025-02-15 20:26:50 +0100

    wrk page

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -1,8 +1,138 @@
+"use client";
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import Link from "next/link";
+import Image from "next/image";
+
+const projects = [
+  {
+    num: "01",
+    category: "frontend",
+    title: "project 1",
+    description:
+      "A modern and responsive static website built using HTML5, CSS3, and JavaScript. This project showcases interactive UI elements, smooth animations, and a mobile-friendly layout, making it an ideal example of clean and structured frontend development.",
+    stack: [
+      {
+        name: "Html 5",
+      },
+      {
+        name: "CSS 3",
+      },
+      {
+        name: "Javascript",
+      },
+    ],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "02",
+    category: "fullstack",
+    title: "project 2",
+    description:
+      "A full-stack web application developed using Next.js for the frontend, Tailwind CSS for styling, and Node.js for the backend. This project integrates dynamic content, server-side rendering, and API connections, ensuring fast performance and a seamless user experience.",
+    stack: [
+      {
+        name: "Next.js",
+      },
+      {
+        name: "Tailwind.css",
+      },
+      {
+        name: "Node.js",
+      },
+    ],
+    image: "/assets/work/thumb2.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "03",
+    category: "frontend",
+    title: "project 3",
+    description:
+      "A sleek, modern frontend project built with Next.js and styled using Tailwind CSS. This project demonstrates a minimalist UI with optimized performance, dynamic routing, and reusable components, making it scalable and maintainable.",
+    stack: [
+      {
+        name: "Next.js",
+      },
+      {
+        name: "Tailwind.css",
+      },
+    ],
+    image: "/assets/work/thumb3.png",
+    live: "",
+    github: "",
+  },
+];
 
 const WorkPage = () => {
+  const [project, setProject] = useState(projects[0]);
   return (
-    <div>Work Page</div>
-  )
-}
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className=" flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div className="">
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                {project.num}
+              </div>
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                {project.category} project
+              </h2>
 
-export default WorkPage
+              <p className="text-white/60">{project.description}</p>
+
+              <ul className=" flex gap-4">
+                {project.stack.map((item, index) => {
+                  return (
+                    <li className="text-xl text-accent" key={index}>
+                      {item.name}
+                      {index !== project.stack.lenght - 1 && ","}
+                    </li>
+                  );
+                })}
+              </ul>
+
+              <div className="border border-white/20"></div>
+              <div className="">
+                <Link href={project.live}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 justify-center">
+                        <BsArrowUpRight />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p className=""> live project</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%]">slider</div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default WorkPage;
