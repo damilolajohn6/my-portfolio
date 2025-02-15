1556c3e0607be8d7f18c5f5541f08d13e47debea
Damilola john <damilolajohn622@gmail.com>
2025-02-15 21:21:29 +0100

    work slide

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { motion } from "framer-motion";
-import React, { useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+import { useState } from "react";
 import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 import {
   Tooltip,
@@ -80,6 +80,12 @@ const projects = [
 
 const WorkPage = () => {
   const [project, setProject] = useState(projects[0]);
+  const handleSlideChange = (swiper) => {
+    const currentIndex = swiper.activeIndex;
+    setProject(projects[currentIndex]);
+  };
+
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -90,7 +96,7 @@ const WorkPage = () => {
       <div className="container mx-auto">
         <div className=" flex flex-col xl:flex-row xl:gap-[30px]">
           <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-            <div className="">
+            <div className="flex flex-col gap-[30px] h-[50%]">
               <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
                 {project.num}
               </div>
@@ -105,18 +111,18 @@ const WorkPage = () => {
                   return (
                     <li className="text-xl text-accent" key={index}>
                       {item.name}
-                      {index !== project.stack.lenght - 1 && ","}
+                      {index !== project.stack.lenght - 1 && ','}
                     </li>
-                  );
+                  )
                 })}
               </ul>
 
               <div className="border border-white/20"></div>
-              <div className="">
+              <div className="flex items-center gap-4 ">
                 <Link href={project.live}>
                   <TooltipProvider delayDuration={100}>
                     <Tooltip>
-                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 justify-center items-center group">
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full flex bg-white/5 justify-center items-center group">
                         <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
                       </TooltipTrigger>
                       <TooltipContent>
@@ -125,14 +131,42 @@ const WorkPage = () => {
                     </Tooltip>
                   </TooltipProvider>
                 </Link>
+
+                <Link href={project.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full flex bg-white/5 justify-center items-center group">
+                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p className=""> Github Repository</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
               </div>
             </div>
           </div>
-          <div className="w-full xl:w-[50%]">slider</div>
+          <div className="w-full xl:w-[50%]">
+            <Swiper spaceBetween={30} slidesPerView={1} className="xl:h-[520px] mb-12"  onSlideChange={handleSlideChange}>
+              {projects.map((project, index) => {
+                return <SwiperSlide key={index} className="w-full">
+                  <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+
+                    <div className=""></div>
+                    <div className="relative w-full h-full">
+                      <Image src={project.image} fill className='object-cover' alt="" />
+                    </div>
+
+                  </div>
+                </SwiperSlide>
+              })}
+            </Swiper>
+          </div>
         </div>
       </div>
     </motion.div>
-  );
+  )
 };
 
 export default WorkPage;
