6b72304adc351a1a144bdbae097a2b1f74e6b438
Damilola john <97457114+damilolajohn6@users.noreply.github.com>
2025-03-21 03:02:14 +0100

    cv fixed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -21,7 +21,7 @@ const Home = () => {
               proficient in various programming languages and technologies.
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <a href="https://damilolaresume.tiiny.site">
+              <a href="https://damilola-resume.tiiny.site">
                 <Button
                   variant="outline"
                   size="lg"
